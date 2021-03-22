7c2d289f21618b2bde61ee7a3073f00cbe651103
Rafael Lopez <rafael.lopez@bbd.ca>
2021-03-21 21:45:48 -0400

    Add Total to Cart component

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -9,12 +9,20 @@ import {Product} from '../../models/Product';
 })
 export class CartComponent implements OnInit {
   products: Product[];
+  total = 0;
 
   constructor(private cartService: CartService) {
     this.cartService.sharedCart.subscribe(cart => this.products = cart);
   }
 
   ngOnInit(): void {
+    this.calculateTotal();
+  }
+
+  calculateTotal = () => {
+    this.products.forEach( product => {
+      this.total += product.price;
+    });
   }
 
 }
